06f3f7106c8684a3b237342f34139eaedc693fea
legendary-cookie <github.com@xolley.de>
2021-11-13 17:21:04 +0100

    Added autosuggestion plugin

--- plugins/plugins.zsh
+++ plugins/plugins.zsh
@@ -4,3 +4,5 @@ source $DOTFILES/plugins/zsh-plugin-appup/appup.plugin.zsh
 source $DOTFILES/plugins/zsh-dotnet-completion/zsh-dotnet-completion.plugin.zsh
 source $DOTFILES/plugins/zsh-github-cli/cli.plugin.zsh
 source $DOTFILES/plugins/gradle-zsh-completion/gradle-zsh-completion.plugin.zsh
+source $DOTFILES/plugins/autofortune-zsh/auto-fortune-cowsay.plugin.zsh
+source $DOTFILES/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
